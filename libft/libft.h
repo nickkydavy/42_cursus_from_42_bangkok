31ea0baa16c3ddfb63a51965bd1381b2e12151c3
nickkydavy <p.nimwattana@gmail.com>
2022-02-18 14:14:58 +0700

    add prototype ft_memchr in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -45,8 +45,9 @@ void	ft_purchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-size_t	ft_strlen(char const *s);
+void	*ft_memchr(void const *s, int c, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strlen(char const *s);
 size_t	ft_strlcpy(char *dest, char const *src, size_t n);
 size_t	ft_strlcat(char *dest, char const *src, size_t n);
 
