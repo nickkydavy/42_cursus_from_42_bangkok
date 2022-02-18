11a747ebd1964c2cfb9ecd6faf5ecd06e51f2120
nickkydavy <p.nimwattana@gmail.com>
2022-02-18 17:04:06 +0700

    edit ft_memcmp.c to get unsigned char

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -15,16 +15,16 @@
 int	ft_memcmp(void const *s1, void const *s2, size_t n)
 {
 	size_t				i;
-	char	*str1;
-	char	*str2;
+	unsigned	char	*str1;
+	unsigned	char	*str2;
 	
-	str1 = (char *)s1;
-	str2 = (char *)s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
 		if (*(str1 + i) != *(str2 + i))
-			return (*(str2 + i) - *(str1 + i));
+			return (*(str1 + i) - *(str2 + i));
 		i++;
 	}
 	return (0);
