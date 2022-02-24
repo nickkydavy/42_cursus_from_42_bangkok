e310c29f81fea9c8f6857d787b8ea109d28998b6
nickkydavy <p.nimwattana@gmail.com>
2022-02-24 13:38:37 +0700

    add ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -14,6 +14,19 @@
 
 size_t	ft_strlcat(char *dest, char const *src, size_t n)
 {
-	
-	return (0);
+	size_t	i;
+	size_t	len_dest;
+	size_t	len_src;
+
+	len_dest = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	if (n - 1 < len_dest)
+		return (len_src + n);
+	while (len_dest + i < n - 1)
+	{
+		dest[len_dest + i] = src[i];
+		i++;
+	}
+	dest[len_dest + i] = '\0';
+	return (len_dest + i);
 }
