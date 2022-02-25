a3031aa9b8f429e07c554e4fce7523a56a59b247
nickkydavy <p.nimwattana@gmail.com>
2022-02-25 22:42:35 +0700

    edit strlcpy in pc

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -15,11 +15,8 @@
 size_t	ft_strlcpy(char *dest, char const *src, size_t n)
 {
 	size_t	i;
-	size_t	max;
 
 	i = -1;
-	if (n > sizeof (*dest))
-		max = sizeof (0);
 	while (*(dest + i) != '\0' && *(src + i) != '\0' && i < n - 1)
 	{
 		*(dest + i) = *(src + i);
