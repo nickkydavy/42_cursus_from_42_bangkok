ad6551bcd18f77babf30ed33b15d1582cb3e95fe
nickkydavy <p.nimwattana@gmail.com>
2022-02-28 16:46:19 +0700

    edit lstmap and libft.h

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -12,7 +12,37 @@
 
 #include "libft.h"
 
+t_list	*cre_new_lst(t_list *new_lst, void *(*f)(void *), void (*del)(void *), t_list *lst)
+{
+	t_list	*temp;
+
+	new_lst = ft_lstnew(f(lst->content));
+	if (!temp)
+		return (0);
+	temp = new_lst;
+	lst = lst->next;
+	while (lst != 0)
+	{
+		temp->next = ft_lstnew(f(lst->content));
+		if (!temp->next)
+		{
+			ft_lstclear(&new_lst, del);
+			return (0);
+		}
+		temp = temp->next;
+		lst = lst->next;
+	}
+	return (new_lst);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new_lst;
+	t_list	*temp;
+
+	if (!lst || !f || !del)
+		return (0);
+	new_lst = 0;
+	new_lst = cre_new_lst(new_lst, f, del, lst);
+	return (new_lst);
 }
